27536d0ffdbac0a87c6c5a721228fa7767239395
Adam O'Neill <adam.oneill@mercedes-benz.io>
2018-05-31 16:18:22 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+
+module.exports = [{
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+},
+{
+  entry: './src/app.scss',
+  output: {
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'style-bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: 'bundle.css',
+          },
+        },
+        { loader: 'extract-loader' },
+        { loader: 'css-loader' },
+        { loader: 'sass-loader' },
+      ]
+    }]
+  },
+}];
